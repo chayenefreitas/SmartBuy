c2b4d5f48aee2c46360d48e4379aea3518844126
Chayene Freitas <Chayenne_Freitas@hotmail.com>
2025-04-04 15:03:02 -0300

    Aplicando migrations

--- SmartBuy.Gestao/Data/ApplicationDbContext.cs
+++ SmartBuy.Gestao/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SmartBuy.Models;
 
 namespace SmartBuy.Gestao.Data
 {
@@ -9,5 +10,39 @@ namespace SmartBuy.Gestao.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Vendedor>()
+                .Property(p => p.Nome)
+                .HasMaxLength(200);
+
+            builder.Entity<Categoria>()
+                .Property(p => p.Descricao)
+                .HasMaxLength(150);
+
+            builder.Entity<Produto>()
+                .Property(p => p.Preco)
+                .HasPrecision(18, 4);
+
+            builder.Entity<Produto>()
+                .Property(p => p.Estoque)
+                .HasPrecision(18, 4);
+
+            builder.Entity<Produto>()
+                .Property(p => p.Nome)
+                .HasMaxLength(150);
+
+            builder.Entity<Produto>()
+                .Property(p => p.Descricao)
+                .HasMaxLength(250);
+
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Vendedor> Vendedores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
