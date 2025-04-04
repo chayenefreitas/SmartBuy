dce2de005339d749a6d8ba6d222f1e6df36ec087
Chayene Freitas <Chayenne_Freitas@hotmail.com>
2025-04-04 14:57:26 -0300

    .

--- SmartBuy.Model/Models/Vendedor.cs
+++ SmartBuy.Model/Models/Vendedor.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartBuy.Models
 {
@@ -10,23 +11,23 @@ namespace SmartBuy.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Nome { get; set; }
 
-        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
-        //[Display(Name = "E-mail")]
-        //public string Email { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
 
-        //[Display(Name = "Confirme o {0}")]
-        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
-        //[Compare("E-mail", ErrorMessage = "Os e-mails não coincidem")]
-        //[NotMapped]
-        //public string EmailConfirmacao { get; set; }
+        [Display(Name = "Confirme o {0}")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Compare("E-mail", ErrorMessage = "Os e-mails não coincidem")]
+        [NotMapped]
+        public string EmailConfirmacao { get; set; }
 
-        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
-        //public string Senha { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Senha { get; set; }
 
-        //[Display(Name = "Confirme a senha")]
-        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
-        //[Compare("Senha", ErrorMessage = "As senhas não coincidem")]
-        //[NotMapped]
-        //public string SenhaConfirmacao { get; set; }
+        [Display(Name = "Confirme a senha")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Compare("Senha", ErrorMessage = "As senhas não coincidem")]
+        [NotMapped]
+        public string SenhaConfirmacao { get; set; }
     }
 }
